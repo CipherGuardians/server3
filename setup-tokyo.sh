896035905a401125ce4e45b4fb9f5e4a8882d1d3
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2025-04-23 17:29:50 +0200

    Update setup-tokyo.sh

--- setup-tokyo.sh
+++ setup-tokyo.sh
@@ -14,7 +14,7 @@ cat > /etc/shadowsocks-rust/config.json <<EOF
     "server": "::",
     "server_port": 3443,
     "method": "2022-blake3-aes-256-gcm",
-    "password": "bryVZSxD3E55dzwVbVQCtLk67/4Id+FVcf8YHTk49y0=",
+    "password": "P7vKFWwtDhJN/soYsKiun2sXMElDg8RKuzpQUToW20E=",
     "mode": "tcp_and_udp"
 }
 EOF
